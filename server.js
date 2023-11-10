216de7313705f6abe5cb51b47bba64a8589a6d90
henrytrieu88@gmail.com <henrytrieu88@gmail.com>
2023-11-09 20:17:40 -0800

    Updated server.js
    Fixed typos and removed some comments

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('dotenv').config() // loads environment variables from out .env
+require('dotenv').config() // loads environment variables from our .env
 
 const express = require('express') // pulls in express library
-const app = express() // Creates an app variable that we'll use to configure our server
+const app = express() // creates an app variable that we'll use to configure our server
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }) // need to pull this out to an env variable because cannot be used in
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error)) // allows us to if there was an error connecting to the db
 db.once('open', () => console.log('Connected to Database'))
